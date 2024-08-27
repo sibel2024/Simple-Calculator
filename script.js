300e84e80989dfbc2c8ac57584e903ef394cb0df
sibel2024 <sibel.ucar2020@gmail.com>
2024-08-27 23:26:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function appendToDisplay(value){
+    const display= document.getElementById('display');
+    if (display.innerText === '0' && value !== '.') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function deleteLast () {
+    const display = document.getElementById('display');
+    display.innerText = display.innerText.slice(0, -1)  || '0';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try{
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
